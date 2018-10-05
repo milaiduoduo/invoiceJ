66c50e2b5cb4fa7da725bb5228edbba626989812
advancingKoala <milaiduoduo4567@163.com>
2018-10-05 20:52:17 +0800

    完善ui

--- front-admin1/src/views/invoiceBlue.vue
+++ front-admin1/src/views/invoiceBlue.vue
@@ -1,53 +1,113 @@
 <template>
 <div>
-    <el-form ref="form" :model="form" label-width="120px">
-        <el-form-item label="发票代码">
-            <el-input></el-input>
-        </el-form-item>
+    <el-card>
+        <div slot="header" class="headerWrap clearfix">
+				<div>
+					<el-button type="primary" size="medium" @click="onSubmit">立即上传蓝票</el-button>
+                    <el-button type="default" size="small">取消</el-button>
+				</div>
+		</div>
+   
+    <el-form class="formWrap" size="small" ref="form" :model="form" label-width="120px">
+       
+        <el-row>
+            <el-col :offset="1" :span="10">
+                <el-form-item label="发票代码">
+                    <el-input></el-input>
+                </el-form-item>
+            </el-col>
+            <el-col :offset="1" :span="10">
                 <el-form-item label="发票号码">
-            <el-input></el-input>
-        </el-form-item>
+                    <el-input></el-input>
+                </el-form-item>
+            </el-col>
+                
+        </el-row>
+        <el-row> 
+            <el-col :offset="1" :span="10">
                 <el-form-item label="发票抬头">
-            <el-input></el-input>
-        </el-form-item>
+                    <el-input></el-input>
+                </el-form-item>
+            </el-col>
+            <el-col :offset="1" :span="10">
                 <el-form-item label="订单编号">
-            <el-input></el-input>
-        </el-form-item>
+                    <el-input></el-input>
+                </el-form-item>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :offset="1" :span="10">
                 <el-form-item label="开票金额">
-            <el-input></el-input>
-        </el-form-item>
+                    <el-input></el-input>
+                </el-form-item>
+            </el-col>
+            <el-col :offset="1" :span="10">
                 <el-form-item label="开票时间">
-            <el-input></el-input>
-        </el-form-item>
-                <el-form-item label="销货方识别号（税号）">
-            <el-input></el-input>
-        </el-form-item>
+                    <el-date-picker v-model="value1" type="date" placeholder="选择日期">
+    </el-date-picker>
+                </el-form-item>
+            </el-col>
+        </el-row>   
+        <el-row>
+            <el-col :offset="1" :span="10"> 
+                <el-form-item label="销货方税号">
+                    <el-input></el-input>
+                </el-form-item>
+            </el-col>
+            <el-col :offset="1" :span="10">
                 <el-form-item label="销货方公司名称">
-            <el-input></el-input>
-        </el-form-item>
+                    <el-input></el-input>
+                </el-form-item>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :offset="1" :span="10">
                 <el-form-item label="销货方公司地址">
-            <el-input></el-input>
-        </el-form-item>
+                    <el-input></el-input>
+                </el-form-item>
+            </el-col>
+            <el-col :offset="1" :span="10">
                 <el-form-item label="销货方电话">
-            <el-input></el-input>
-        </el-form-item>
-        <el-form-item label="销货方开户行">
-            <el-input></el-input>
-        </el-form-item>
-        <el-form-item label="销货方银行账户">
-            <el-input></el-input>
-        </el-form-item>
-        <el-form-item label="发票备注">
-            <el-input></el-input>
-        </el-form-item>
-        <el-form-item label="开票人">
-            <el-input></el-input>
-        </el-form-item>
-        <el-form-item label="收款人">
-            <el-input></el-input>
-        </el-form-item>
-        <el-form-item label="PDF上传">
-            <el-upload
+                    <el-input></el-input>
+                </el-form-item>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :offset="1" :span="10">
+                <el-form-item label="销货方开户行">
+                    <el-input></el-input>
+                </el-form-item>
+            </el-col>
+            <el-col :offset="1" :span="10">
+                <el-form-item label="销货方银行账户">
+                    <el-input></el-input>
+                </el-form-item>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :offset="1" :span="10">
+                <el-form-item label="开票人">
+                    <el-input></el-input>
+                </el-form-item>
+            </el-col>
+            <el-col :offset="1" :span="10">
+                <el-form-item label="收款人">
+                    <el-input></el-input>
+                </el-form-item>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :offset="1" :span="21">
+                <el-form-item label="发票备注">
+                    <el-input type="textarea" :rows="2" placeholder="请输入内容" v-model="textarea"></el-input>
+                </el-form-item>
+            </el-col>
+            
+        </el-row>
+         <el-row class="uploadWrap">
+            <el-col :offset="1">
+                <el-form-item label="PDF上传">
+                    <el-upload
   class="upload-demo"
   action="https://jsonplaceholder.typicode.com/posts/"
   :on-preview="handlePreview"
@@ -57,15 +117,15 @@
   :limit="3"
   :on-exceed="handleExceed"
   :file-list="fileList">
-  <el-button size="small" type="primary">点击上传</el-button>
-  <div slot="tip" class="el-upload__tip">只能上传pdf文件，且大小不能超过500kb</div>
-</el-upload>
-        </el-form-item>
-        <el-form-item>
-    <el-button type="primary" @click="onSubmit">立即上传蓝票</el-button>
-    <el-button>取消</el-button>
-  </el-form-item>
+                    <el-button size="small" type="primary">点击上传</el-button>
+                    <div slot="tip" class="el-upload__tip">只能上传pdf文件，且大小不能超过500kb</div>
+                    </el-upload>
+                </el-form-item>
+            </el-col>
+        </el-row>
     </el-form>
+
+</el-card>
 </div>
 </template>
 
@@ -74,4 +134,7 @@ export default {};
 </script>
 
 <style scoped lang="scss" rel="stylesheet/scss">
+.headerWrap {
+  padding-left: 30px;
+}
 </style>
--- front-admin1/src/views/invoiceRed.vue
+++ front-admin1/src/views/invoiceRed.vue
@@ -1,27 +1,53 @@
 <template>
 <div>
-        <el-form ref="form"  label-width="120px">
-        <el-form-item label="发票代码">
-            <el-input></el-input>
-        </el-form-item>
+    <el-card>
+        <div slot="header" class="headerWrap clearfix">
+				<div>
+					<el-button type="primary" size="medium" @click="onSubmit">立即上传红票</el-button>
+                    <el-button type="default" size="small">取消</el-button>
+				</div>
+		</div>
+        <el-form class="formWrap" size="small" ref="form" :model="form" label-width="120px">
+        <el-row>
+            <el-col :offset="1" :span="10">
+                <el-form-item label="发票代码">
+                    <el-input></el-input>
+                </el-form-item>
+            </el-col>
+            <el-col :offset="1" :span="10">
                 <el-form-item label="发票号码">
-            <el-input></el-input>
-        </el-form-item>
-                
+                    <el-input></el-input>
+                </el-form-item>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :offset="1" :span="10">
                 <el-form-item label="订单编号">
-            <el-input></el-input>
-        </el-form-item>
+                    <el-input></el-input>
+                </el-form-item>
+            </el-col>
+            <el-col :offset="1" :span="10">
                 <el-form-item label="开票时间">
-            <el-input></el-input>
-        </el-form-item>
+                    <el-input></el-input>
+                </el-form-item>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :offset="1" :span="10">
                 <el-form-item label="原蓝票发票代码">
-            <el-input></el-input>
-        </el-form-item>
+                    <el-input></el-input>
+                </el-form-item>
+            </el-col>
+            <el-col :offset="1" :span="10">
                 <el-form-item label="原蓝票发票号码">
-            <el-input></el-input>
-        </el-form-item>
-        <el-form-item label="PDF上传">
-            <!-- <el-upload
+                    <el-input></el-input>
+                </el-form-item>
+            </el-col>
+        </el-row>    
+        <el-row>
+            <el-col :offset="1" :span="21">
+                <el-form-item label="PDF上传">
+                    <el-upload
   class="upload-demo"
   action="https://jsonplaceholder.typicode.com/posts/"
   :on-preview="handlePreview"
@@ -31,15 +57,16 @@
   :limit="3"
   :on-exceed="handleExceed"
   :file-list="fileList">
-  <el-button size="small" type="primary">点击上传</el-button>
-  <div slot="tip" class="el-upload__tip">只能上传pdf文件，且大小不能超过500kb</div>
-</el-upload> -->
-        </el-form-item>
-        <el-form-item>
-    <el-button type="primary" @click="onSubmit">立即上传红票</el-button>
-    <el-button>取消</el-button>
-  </el-form-item>
+                    <el-button size="small" type="primary">点击上传</el-button>
+                    <div slot="tip" class="el-upload__tip">只能上传pdf文件，且大小不能超过500kb</div>
+                    </el-upload>
+                </el-form-item>
+            </el-col>
+        </el-row>    
+    
     </el-form>
+    </el-card>
+       
 </div>
 </template>
 
@@ -60,4 +87,7 @@ export default {
 </script>
 
 <style scoped lang="scss" rel="stylesheet/scss">
+.headerWrap {
+  padding-left: 30px;
+}
 </style>
